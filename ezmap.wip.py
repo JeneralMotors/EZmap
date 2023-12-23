a246fac7b3348a991201c20ad35c82da6f8fe64a
JeneralMotors <60910202+JeneralMotors@users.noreply.github.com>
2023-12-23 06:35:52 +0100

    Update ezmap.wip.py

--- ezmap.wip.py
+++ ezmap.wip.py
@@ -1,65 +1,27 @@
-import argparse
-import nmap
-import ipaddress
 import sys
+import nmap
 
-def expand_ip_range(ip_range):
-    try:
-        # Expands a given IP range in CIDR notation to a list of individual IP addresses
-        ip_list = [str(ip) for ip in ipaddress.IPv4Network(ip_range, strict=False)]
-        return ip_list
-    except ValueError as e:
-        print(f"Error expanding IP range: {e}")
-        sys.exit(1)
-
-def scan_target(target):
-    try:
-        # Create a Nmap object
-        nm = nmap.PortScanner()
-
-        # Perform a host discovery using multiple methods (-sn for ping scan, -PR for ARP ping)
-        nm.scan(target, arguments='-sn -PR')
-
-        # Iterate through discovered hosts and perform a detailed port scan
-        for host in nm.all_hosts():
-            print(f"Host: {host}")
-            # Perform a detailed port scan on the discovered host
-            nm.scan(host, arguments='-p 1-1000')
-
-            # Print information about open ports on the host
-            for proto in nm[host].all_protocols():
-                print(f"Protocol: {proto}")
-                ports = nm[host][proto].keys()
-                for port in ports:
-                    print(f"Port: {port}, State: {nm[host][proto][port]['state']}")
-    except nmap.PortScannerError as e:
-        print(f"Nmap scan error: {e}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+def scan_ports(ip):
+    scanner = nmap.PortScanner()
+    scanner.scan(ip, arguments='-p 1-65535 --open -sS -n -Pn -sV')  # Added -sV for service version detection
+    
+    for host in scanner.all_hosts():
+        print(f"Open ports for {host}:")
+        for proto in scanner[host].all_protocols():
+            ports = scanner[host][proto].keys()
+            for port in ports:
+                service_name = scanner[host][proto][port]['name']
+                service_state = scanner[host][proto][port]['state']
+                service_product = scanner[host][proto][port]['product']
+                service_version = scanner[host][proto][port]['version']
+                
+                print(f"Port {port}: {service_name} - {service_state}")
+                print(f"  Service: {service_product} {service_version}")
 
 if __name__ == "__main__":
-    try:
-        # Create an ArgumentParser object to handle command-line arguments
-        parser = argparse.ArgumentParser(description="Nmap Scanner with Host Discovery")
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <IP>")
+        sys.exit(1)
 
-        # Add a required argument for the target IP address, range, or CIDR notation
-        parser.add_argument("target", help="Specify an IP address, range, or CIDR notation to scan")
-
-        # Parse the command-line arguments
-        args = parser.parse_args()
-        target = args.target
-
-        # If the input is in CIDR notation, expand it to a list of individual IP addresses
-        if '/' in target:
-            ip_list = expand_ip_range(target)
-
-            # Scan each individual IP address in the expanded list
-            for ip in ip_list:
-                scan_target(ip)
-        else:
-            # If the input is a single IP address or range, scan it directly
-            scan_target(target)
-    except argparse.ArgumentError as e:
-        print(f"Argument error: {e}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+    target_ip = sys.argv[1]
+    scan_ports(target_ip)
